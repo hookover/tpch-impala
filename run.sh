d1e4047158405a8337914ddb4acee5d557143467
ChenJiang <chenjiang1@superatomfin.com>
2019-03-28 11:03:00 +0800

    增加 presto 脚本命令

--- run.sh
+++ run.sh
@@ -34,28 +34,39 @@ while [ $trial -lt $NUM_OF_TRIALS ]; do
 #		fi
 
 		# If you want to use old beta, enable below.
-		#$TIME_CMD $IMPALA_CMD -q 'refresh' 2>&1 | tee -a $LOG_FILE | grep '^Time:'
-                #returncode=${PIPESTATUS[0]}
-		#if [ $returncode -ne 0 ]; then
-		#	echo "ABOVE QUERY FAILED:$returncode"
-		#fi
+#		$TIME_CMD $IMPALA_CMD -q 'refresh' 2>&1 | tee -a $LOG_FILE | grep '^Time:'
+#                returncode=${PIPESTATUS[0]}
+#		if [ $returncode -ne 0 ]; then
+#			echo "ABOVE QUERY FAILED:$returncode"
+#		fi
 
-		echo "Running Impala query: $query" >> $LOG_FILE
-		$TIME_CMD $IMPALA_CMD --query_file=$BASE_DIR/$TPCH_QUERIES_SUB_PATH/${query}.impala 2>&1 | tee -a $LOG_FILE | grep '^Time:'
+#		echo "Running Impala query: $query" >> $LOG_FILE
+#		$TIME_CMD $IMPALA_CMD --query_file=$BASE_DIR/$TPCH_QUERIES_SUB_PATH/${query}.impala 2>&1 | tee -a $LOG_FILE | grep '^Time:'
+#                returncode=${PIPESTATUS[0]}
+#		if [ $returncode -ne 0 ]; then
+#			echo "ABOVE QUERY FAILED:$returncode"
+#		fi
+#		echo "" >> $LOG_FILE
+#		echo "Impala query: $query Done" >> $LOG_FILE
+#		echo "" >> $LOG_FILE
+
+		echo "Running Presto query: $query" >> $LOG_FILE
+		$TIME_CMD $PRESTO_CMD --file=$BASE_DIR/$TPCH_QUERIES_SUB_PATH/${query}.presto 2>&1 | tee -a $LOG_FILE | grep '^Time:'
                 returncode=${PIPESTATUS[0]}
 		if [ $returncode -ne 0 ]; then
 			echo "ABOVE QUERY FAILED:$returncode"
 		fi
 		echo "" >> $LOG_FILE
-		echo "Impala query: $query Done" >> $LOG_FILE
+		echo "Presto query: $query Done" >> $LOG_FILE
 		echo "" >> $LOG_FILE
 
-		#echo "Running Hive query: $query" >> $LOG_FILE
-		#$TIME_CMD $HIVE_CMD -f $BASE_DIR/tpch_hive/${query}.hive 2>&1 | tee -a $LOG_FILE | grep '^Time:'
-                #returncode=${PIPESTATUS[0]}
-		#if [ $returncode -ne 0 ]; then
-		#	echo "ABOVE QUERY FAILED:$returncode"
-		#fi
+
+#		echo "Running Hive query: $query" >> $LOG_FILE
+#		$TIME_CMD $HIVE_CMD -f $BASE_DIR/tpch_hive/${query}.hive 2>&1 | tee -a $LOG_FILE | grep '^Time:'
+#                returncode=${PIPESTATUS[0]}
+#		if [ $returncode -ne 0 ]; then
+#			echo "ABOVE QUERY FAILED:$returncode"
+#		fi
 	done
 
 done # TRIAL
